f668aeaa38a6975126eafd9a967fefe79f904cd5
mmaaat <mmaaat@gmail.com>
2026-09-09 14:52:30 +0200

    Dodano proszkownie

---
+++ docs/2026-06-00_Proszkownia_All/cloud.js
@@ -0,0 +1,33 @@
+{
+    "version": "1.7.3",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 70935431,
+    "boundingBox": {
+        "lx": -20.402747802734376,
+        "ly": -16.25082275390625,
+        "lz": -4.441719055175781,
+        "ux": 19.44946319580078,
+        "uy": 23.601388244628909,
+        "uz": 35.410491943359378
+    },
+    "tightBoundingBox": {
+        "lx": -20.402747802734376,
+        "ly": -16.25082275390625,
+        "lz": -4.441719055175781,
+        "ux": 19.44946319580078,
+        "uy": 22.85469940185547,
+        "uz": 17.36827850341797
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 0.3451302647590637,
+    "scale": 0.001,
+    "hierarchyStepSize": 5,
+    "pointClassCount": {
+        "0": 71233877
+    }
+}

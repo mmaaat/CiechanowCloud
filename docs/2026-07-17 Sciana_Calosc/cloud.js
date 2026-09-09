5d1fb611ef2c5b9934f492cc8f1bf834b467f8c7
mmaaat <mmaaat@gmail.com>
2026-09-09 12:18:53 +0200

    Dodanie sciana calosc

---
+++ docs/2026-07-17 Sciana_Calosc/cloud.js
@@ -0,0 +1,33 @@
+{
+    "version": "1.7.3",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 9999836,
+    "boundingBox": {
+        "lx": -43.60611324342906,
+        "ly": -71.87820324725533,
+        "lz": 62.368425653233497,
+        "ux": 81.98914095712118,
+        "uy": 53.717050953294918,
+        "uz": 187.96367985378374
+    },
+    "tightBoundingBox": {
+        "lx": -25.65740203857422,
+        "ly": -25.281892776489259,
+        "lz": 75.10128784179688,
+        "ux": 6.250827789306641,
+        "uy": 1.8159161806106568,
+        "uz": 93.66455078125
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 1.0876867771148682,
+    "scale": 0.001,
+    "hierarchyStepSize": 5,
+    "pointClassCount": {
+        "0": 294796072
+    }
+}

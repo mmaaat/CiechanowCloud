05ef17030b3a04a32b7fc78991fd64a6abf80152
mmaaat <mmaaat@gmail.com>
2026-09-09 16:18:03 +0200

    Zbiorniki wiezy ustawione

---
+++ docs/2026-09-09_Zbiorniki/cloud.js
@@ -0,0 +1,33 @@
+{
+    "version": "1.7.3",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 27790534,
+    "boundingBox": {
+        "lx": -12.258794628382998,
+        "ly": 1.7791562006629119,
+        "lz": 0.6150005437437596,
+        "ux": 1.285205997227349,
+        "uy": 15.323156826273259,
+        "uz": 14.159001169354106
+    },
+    "tightBoundingBox": {
+        "lx": -12.258794628382998,
+        "ly": 1.7791562006629119,
+        "lz": 0.6150005437437596,
+        "ux": -6.29309347471631,
+        "uy": 7.71375260115574,
+        "uz": 14.159001169354106
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 0.11729448288679123,
+    "scale": 0.001,
+    "hierarchyStepSize": 5,
+    "pointClassCount": {
+        "0": 27790534
+    }
+}
